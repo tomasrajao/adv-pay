88811dd250984a81dac46746ec1e9451a9cfff1e
tomasrajao <tomasrajao@gmail.com>
2021-10-10 22:02:04 -0300

    Testes da homepage

--- base/tests/test_home.py
+++ base/tests/test_home.py
@@ -4,6 +4,16 @@ from model_bakery import baker
 
 from advpay.django_assertions import assert_contains
 
+
+@pytest.fixture
+def resp(client, db):
+    return client.get(reverse('login'))
+
+
+def test_login_form_page(resp):
+    assert resp.status_code == 200
+
+
 @pytest.fixture
 def user(db, django_user_model):
     user_model = baker.make(django_user_model)
@@ -12,10 +22,6 @@ def user(db, django_user_model):
     user_model.save()
     user_model.flat_password = password
     return user_model
-
-@pytest.fixture
-def resp(client, user):
-    return client.post(reverse('login'), {'username': user.email, 'password': user.flat_password})
 
 
 @pytest.fixture
@@ -31,17 +37,11 @@ def test_logout_button(resp_home):
     assert_contains(resp_home, 'Sair')
 
 
-
 @pytest.fixture
-def resp_post(client, db):
-    return client.post(reverse('login'))
-
-
-def test_login_page(resp_post):
-    assert resp_post.status_code == 200
+def resp_post(client, user, db):
+    return client.post(reverse('login'), {'username': user.username, 'password': user.flat_password})
 
 
 def test_login_redirect(resp_post):
     assert resp_post.status_code == 302
-    assert resp_post.url == reverse('base/home.html')
-
+    assert resp_post.url == reverse('base:home')
