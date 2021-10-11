a4028b732f0c39582cd1e909b301683eb57c4d0a
tomasrajao <tomasrajao@gmail.com>
2021-10-10 21:35:02 -0300

    Login obrigatório para acesso ao sistema

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+@login_required
 def home(request):
-    return render(request, 'registration/home.html')
+    return render(request, 'base/home.html')
